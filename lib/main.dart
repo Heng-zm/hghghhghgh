7fbe32b55b71c4b2593030162b3219f34f79049c
unknown <hengk7401@gmail.com>
2025-01-08 18:45:26 +0700

    K

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Camera',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: CameraPage(),
-    );
-  }
-}
-
-class CameraPage extends StatefulWidget {
-  @override
-  _CameraPageState createState() => _CameraPageState();
-}
-
-class _CameraPageState extends State<CameraPage> {
-  late CameraController _controller;
-  late Future<void> _initializeControllerFuture;
-  late List<CameraDescription> _cameras;
-
-  @override
-  void initState() {
-    super.initState();
-    _initializeCamera();
-  }
-
-  // Initialize the camera
-  void _initializeCamera() async {
-    _cameras = await availableCameras();
-    _controller = CameraController(
-      _cameras[0], // Use the first camera
-      ResolutionPreset.high,
-    );
-    _initializeControllerFuture = _controller.initialize();
-    setState(() {});
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _controller.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Camera Example')),
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return CameraPreview(_controller);
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+            child: WebView(
+          initialUrl: "https://heng-zm.github.io/tretr.github.io/oo.html",
+          javascriptMode: JavascriptMode.unrestricted,
+        )),
       ),
     );
   }
